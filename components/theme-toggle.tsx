d514c1dcc2d60564c11a5e21f58b31ffe9c5fb8f
Somanyu Samal <somanyu.03samal@gmail.com>
2023-10-28 19:01:01 +0530

    feat: Use shadcn/ui and next-themes

---
+++ components/theme-toggle.tsx
@@ -0,0 +1,18 @@
+"use client"
+
+import { MoonIcon, SunIcon } from "@radix-ui/react-icons"
+import { useTheme } from "next-themes"
+
+import { Button } from "@/components/ui/button"
+
+export function ModeToggle() {
+    const { setTheme, theme } = useTheme()
+
+    return (
+        <Button variant="outline" size="icon" onClick={() => setTheme(theme === "light" ? "dark" : "light")}>
+            <SunIcon className="h-[1.5rem] w-[1.3rem] dark:hidden" />
+            <MoonIcon className="hidden h-5 w-5 dark:block" />
+            <span className="sr-only">Toggle theme</span>
+        </Button>
+    )
+}
