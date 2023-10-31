1a7ab0fd6bd497a55690a3462912c8f2e2c08530
Somanyu Samal <somanyu.03samal@gmail.com>
2023-10-31 19:18:39 +0530

    update: Add image domains and set dangerouslyAllowSVG to true

--- next.config.js
+++ next.config.js
@@ -1,4 +1,20 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+    images: {
+        dangerouslyAllowSVG: true,
+        remotePatterns: [
+            {
+                protocol: 'https',
+                hostname: 'cdn.sanity.io',
+                port: '',
+            },
+            {
+                protocol: 'https',
+                hostname: 'api.dicebear.com',
+                port: '',
+            },
+        ],
+    },
+}
 
 module.exports = nextConfig
